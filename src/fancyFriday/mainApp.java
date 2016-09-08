6dcca89411045d6a65c96b263eeae805bd8463d0
David Smiley <DGSmiley77@gmail.com>
2016-09-08 11:24:37 -0400

    first commit

---
+++ src/fancyFriday/mainApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fancyFriday;
+
+/**
+ * @author Student
+ *
+ */
+public class mainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
