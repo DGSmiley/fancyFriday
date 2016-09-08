15fad1a901cbed2db185cfd57258fe8e9fa344f4
David Smiley <DGSmiley77@gmail.com>
2016-09-08 14:11:08 -0400

    options for outfits

--- src/fancyFriday/mainApp.java
+++ src/fancyFriday/mainApp.java
@@ -14,11 +14,13 @@ public class mainApp {
 	
 	
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		
 		String inputFancyFriday, inputSunny, inputRainy;
 		boolean fancyFriday = false;
 		boolean sunny = false;
 		boolean rainy = false;
+		
+		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Is it fancy friday?");
 		 inputFancyFriday = input.nextLine();
@@ -29,7 +31,33 @@ public class mainApp {
 		 System.out.println("is it rainy?");
 		 inputRainy = input.nextLine();
 		 
-		 System.out.println(inputFancyFriday + " " + inputSunny + " " + inputRainy);
+		 // System.out.println(inputFancyFriday + " " + inputSunny + " " + inputRainy);
+		 
+		 if (inputFancyFriday.equals("yes")) {
+			 fancyFriday = true;
+		 }
+		 if (inputSunny.equals("yes")) {
+			 sunny = true;
+		 }
+		 if (inputRainy.equals("yes")) {
+			 rainy = true;
+		 }
+		 
+		 // System.out.println(fancyFriday + " " + sunny + " " + rainy);
+		 
+		 if (fancyFriday){
+			  if (sunny){ 
+				  System.out.println("teagown");
+			 } else if(rainy){
+				 System.out.println("power suit");
+			 } else { 
+				 System.out.println("long gown");
+			 }
+		 } else {
+			 System.out.println("tea shirt and jeans");
+		 }
+			 
+		 
 		 
 		 
 
