21d8e37e92051eb2345975d4a021ceb81764250e
David Smiley <DGSmiley77@gmail.com>
2016-09-08 12:47:14 -0400

    2nd commit

--- src/fancyFriday/mainApp.java
+++ src/fancyFriday/mainApp.java
@@ -3,17 +3,23 @@
  */
 package fancyFriday;
 
+import java.util.Scanner;
+
 /**
  * @author Student
  *
  */
 public class mainApp {
 
-	/**
-	 * @param args
-	 */
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Is it fancy friday?");
+		 String answer = input.nextLine();
+		 
+		 
 
 	}
 
