1218bb8f35d773120ff06b5df1ab32e02bce86ff
David Smiley <DGSmiley77@gmail.com>
2016-09-08 12:49:12 -0400

    3rd commit

--- src/fancyFriday/mainApp.java
+++ src/fancyFriday/mainApp.java
@@ -15,9 +15,19 @@ public class mainApp {
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		String inputFancyFriday, inputSunny, inputRainy;
+		boolean fancyFriday = false;
+		boolean sunny = false;
+		boolean rainy = false;
 		
 		System.out.println("Is it fancy friday?");
-		 String answer = input.nextLine();
+		 inputFancyFriday = input.nextLine();
+		 
+		 System.out.println("Is it sunny?");
+		 inputSunny = input.nextLine();
+		 
+		 System.out.println("is it rainy?");
+		 inputRainy = input.nextLine();
 		 
 		 
 
